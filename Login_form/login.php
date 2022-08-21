a3f57fd5014986dfd4272258c59d9be183d0a204
Suketi17 <nguyenduyhau.dct@gmail.com>
2022-08-21 14:56:20 +0700

    ok file

--- Login_form/login.php
+++ Login_form/login.php
@@ -1,23 +1,26 @@
 <?php 
 	// print_r($_POST);
 	// die();
+	$username = $_POST['username'];
+	$password = $_POST['password'];
 	
-	$hostname = "localhost";
-	$username = "root";
-	$password = "";
-	$databasename = "login_db";
-	$connect = mysqli_connect($hostname,$username,$password,$databasename);
+	$hostnameDB = "localhost";
+	$usernameDB = "root";
+	$passwordDB = "";
+	$databasenameDB = "login_db";
+	$connect = mysqli_connect($hostnameDB,$usernameDB,$passwordDB,$databasenameDB);
 	if (!$connect) {
 		// echo "Connect không thành công!"
 		exit("Connect không thành công!");
 	}
-	$query = "SELECT * FROM login_table"; //query : truy vấn dữ liệu
+	$query = "INSERT INTO login_table (`username`, `pass`)
+	VALUES ($username, $password);";
+	// $query = "SELECT * FROM login_table"; //query : truy vấn dữ liệu
 	$result = mysqli_query($connect, $query); //result : thực thi câu tri vấn
 	echo "<pre>";
-	var_dump(mysqli_fetch_array($result, MYSQLI_BOTH));
+	// var_dump(mysqli_fetch_array($result, MYSQLI_BOTH));
 	echo "</pre>";
 	echo "Connect thành công!"
-
 
 
  ?>
